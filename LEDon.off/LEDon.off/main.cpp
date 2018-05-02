f9cc3ce6cc0881d6435b09fa070eeb5c05c02ccc
sangmin <lsm0378@gmail.com>
2018-05-02 11:48:25 +0900

    LED 0.5초 순차점등

--- LEDon.off/LEDon.off/main.cpp
+++ LEDon.off/LEDon.off/main.cpp
@@ -18,12 +18,12 @@ int main(void)
 		for (int i=0; i <7; i++)
 		{
 			PORTD = ~(1<<i);
-			_delay_ms(300);
+			_delay_ms(500);
 		}
 		for (int i=0; i <7; i++)
 		{
 			PORTD = ~(0x80 >> i);
-			_delay_ms(300);
+			_delay_ms(500);
 		}
     }
 }
